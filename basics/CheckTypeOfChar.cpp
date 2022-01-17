b871b57ec4d1fb830e4cb1c96fd3039b6bb574bb
Pradipta <pradipta.nag2@gmail.com>
2022-01-17 21:58:20 +0530

    Formatting changes

--- basics/CheckTypeOfChar.cpp
+++ basics/CheckTypeOfChar.cpp
@@ -6,16 +6,22 @@ using namespace std;
  * 
  * @return int 
  */
-int main() {
+int main()
+{
     char ch;
     cout << "Enter a Character : " << endl;
     cin >> ch;
-    if(ch >= 97 and ch <= 122) {
+    if (ch >= 97 and ch <= 122)
+    {
         cout << "This is a lowercase Character. " << endl;
-    } else if(ch >= 65 and ch <= 90) {
+    }
+    else if (ch >= 65 and ch <= 90)
+    {
         cout << "This is an Uppercase Character. " << endl;
-    } else {
+    }
+    else
+    {
         cout << "This is a Numeric Character. " << endl;
     }
-	return 0;
+    return 0;
 }
