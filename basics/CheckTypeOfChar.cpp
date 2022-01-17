c202bd32b2d5e725e986607ff34cc9bf0b8ca8b4
Pradipta <pradipta.nag2@gmail.com>
2022-01-17 21:56:12 +0530

    new changes

---
+++ basics/CheckTypeOfChar.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+/**
+ * @brief Check whether a given character is lower case or uppercase or a number
+ * 
+ * @return int 
+ */
+int main() {
+    char ch;
+    cout << "Enter a Character : " << endl;
+    cin >> ch;
+    if(ch >= 97 and ch <= 122) {
+        cout << "This is a lowercase Character. " << endl;
+    } else if(ch >= 65 and ch <= 90) {
+        cout << "This is an Uppercase Character. " << endl;
+    } else {
+        cout << "This is a Numeric Character. " << endl;
+    }
+	return 0;
+}
---
+++ basics/TakeInput.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int keyStroke;
+    keyStroke = cin.get(); //This accepts space, enter, tab etc.
+    cout << "ASCII value : " << keyStroke;
+}
