e7b996f7c884808f39dd5bf596815af552abff34
Pradipta <pradipta.nag2@gmail.com>
2022-01-19 10:08:55 +0530

    Pattern simple questions

---
+++ patterns/Pattern2.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+/**
+ * @brief 
+ * 
+ * 111
+ * 222
+ * 333
+ * 
+ * @return int 
+ */
+int main()
+{
+    int n;
+    cin >> n;
+
+    int i = 1;
+
+    while (i <= n)
+    {
+        int j = 1;
+        while (j <= n)
+        {
+            cout << i;
+            j = j + 1;
+        }
+        cout << endl;
+        i = i + 1;
+    }
+    return 0;
+}
