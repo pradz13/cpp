64faf90b338e67aa496b26ec7bc89a75f74c7303
Pradipta <pradipta.nag2@gmail.com>
2022-01-20 11:08:20 +0530

    New simple pattern

---
+++ patterns/Pattern3.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+/**
+ * @brief 
+ * 1234
+ * 1234
+ * 1234
+ * 
+ * @return int 
+ */
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int i = 1;
+
+    while (i <= n)
+    {
+        int j = 1;
+        while (j <= n)
+        {
+            cout << j;
+            j = j + 1;
+        }
+        cout << endl;
+        i = i + 1;
+    }
+
+    return 0;
+}
